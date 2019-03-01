d8ebd450c067c75f44ff9f13c4def309b389b9e2
Ronny Häfeli <ronny.haefeli@students.fhnw.ch>
2019-03-01 13:57:11 +0100

    Initial Project

---
+++ timecast-backend/src/main/java/wodss/timecastbackend/TimecastBackendApplication.java
@@ -0,0 +1,13 @@
+package wodss.timecastbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimecastBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimecastBackendApplication.class, args);
+	}
+
+}
