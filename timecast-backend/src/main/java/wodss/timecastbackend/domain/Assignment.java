857fd4f2fa53904c87e6e80c06a813c042c2061b
Cagatay Suhan Özyurt <cagatay.oezyurt@students.fhnw.ch>
2019-03-01 17:23:48 +0100

    Several changes:
    - Added javax validation
    - Configured PostgreSQL
    - Defined Project Model
    - Defined Assignment Model

--- timecast-backend/src/main/java/wodss/timecastbackend/domain/Assignment.java
+++ timecast-backend/src/main/java/wodss/timecastbackend/domain/Assignment.java
@@ -1,4 +1,80 @@
 package wodss.timecastbackend.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
 public class Assignment {
+
+    protected Assignment(){}
+
+    @Id
+    private int id;
+    @OneToOne(optional = false)
+    private Project project;
+    @OneToOne(optional = false)
+    private User user;
+    @Max(100)
+    @Min(0)
+    private int employment;
+    @NotNull
+    @NotBlank
+    private LocalDate startDate;
+    @NotNull
+    @NotBlank
+    private LocalDate endDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getEmployment() {
+        return employment;
+    }
+
+    public void setEmployment(int employment) {
+        this.employment = employment;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }
--- timecast-backend/src/main/java/wodss/timecastbackend/domain/Project.java
+++ timecast-backend/src/main/java/wodss/timecastbackend/domain/Project.java
@@ -1,4 +1,73 @@
 package wodss.timecastbackend.domain;
 
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
 public class Project {
+
+    @Id
+    private int id;
+    @NotNull
+    private String name;
+    @NotNull
+    private LocalDateTime startDate;
+    @NotNull
+    private LocalDateTime endDate;
+    @NotNull
+    private LocalDateTime estimatedEndDate;
+    @Min(0)
+    private float ftes;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public LocalDateTime getEstimatedEndDate() {
+        return estimatedEndDate;
+    }
+
+    public void setEstimatedEndDate(LocalDateTime estimatedEndDate) {
+        this.estimatedEndDate = estimatedEndDate;
+    }
+
+    public float getFtes() {
+        return ftes;
+    }
+
+    public void setFtes(float ftes) {
+        this.ftes = ftes;
+    }
+    //TODO: What will be used? FTEs? Hours?
 }
