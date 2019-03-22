6a8645b5d179709e5ae3fb6a300b89349fed61d7
Cagatay Suhan Özyurt <cagatay.oezyurt@students.fhnw.ch>
2019-03-22 16:42:07 +0100

    Fixed ModelMapper

--- timecast-backend/src/main/java/wodss/timecastbackend/util/ModelMapper.java
+++ timecast-backend/src/main/java/wodss/timecastbackend/util/ModelMapper.java
@@ -16,15 +16,20 @@ public interface ModelMapper {
         return r.getId();
     }
 
+    @Mapping(source = "employee", target = "employeeId")
     ContractDTO contractToContractDTO(Contract contract);
+    default Long employeeToLong(Employee e) { return e.getId(); };
+
+    @Mapping(source = "projectManager", target = "projectManagerId")
     ProjectDTO projectToProjectDTO(Project p);
+
     RoleDTO roleToRoleDTO(Role r);
 
     @Mapping(source = "project", target = "projectId")
     @Mapping(source = "contract", target = "contractId")
     AllocationDTO allocationToAllocationDTO(Allocation a);
-    default Long employeeToLong(Employee e){
-        return e.getId();
+    default Long contractToLong(Contract c){
+        return c.getId();
     }
     default Long projectToLong(Project p){
         return p.getId();
