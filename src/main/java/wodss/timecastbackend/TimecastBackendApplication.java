bfc45308d6e8fbfeb11a48cec5789622470c4da0
Cagatay Suhan Özyurt <cagatay.oezyurt@students.fhnw.ch>
2019-05-10 19:44:01 +0200

    Added tomcat configurations

--- src/main/java/wodss/timecastbackend/TimecastBackendApplication.java
+++ src/main/java/wodss/timecastbackend/TimecastBackendApplication.java
@@ -2,9 +2,16 @@ package wodss.timecastbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TimecastBackendApplication {
+public class TimecastBackendApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TimecastBackendApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimecastBackendApplication.class, args);
